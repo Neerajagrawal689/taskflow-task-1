a6e4bbd8975ca6f57d7ef2934d3b86f129784f7d
Neeraj Agrawal <neerajagrawal689@gmail.com>
2025-09-28 08:53:30 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile nav
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
